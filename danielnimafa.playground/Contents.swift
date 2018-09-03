a9d9bb3ed7ea7d1921f2e5aa9f79de81a130a66e
Daniel <danielnimafa@gmail.com>
2018-09-04 06:47:30 +0700

    prepare for loop

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -2,19 +2,21 @@
 
 import UIKit
 
-func calculateBMI(weight: Double, height: Double) -> Double {
-    //let bmi = weight / (height * height)
-    let bmi = weight / pow(height, 2)
-    
-    if bmi > 25 {
-        print("You are overweight")
-    } else if bmi > 18.5 && bmi < 25 {
-        print("You have a normal weight")
-    } else if bmi < 18.5 {
-        print("You are underweight")
-    }
-    
-    return bmi
-}
 
-var bmiResult = calculateBMI(weight: 70, height: 1.63)
+
+//func calculateBMI(weight: Double, height: Double) -> Double {
+//    //let bmi = weight / (height * height)
+//    let bmi = weight / pow(height, 2)
+//
+//    if bmi > 25 {
+//        print("You are overweight")
+//    } else if bmi > 18.5 && bmi < 25 {
+//        print("You have a normal weight")
+//    } else if bmi < 18.5 {
+//        print("You are underweight")
+//    }
+//
+//    return bmi
+//}
+//
+//var bmiResult = calculateBMI(weight: 70, height: 1.63)
