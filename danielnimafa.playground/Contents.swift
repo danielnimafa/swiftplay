8dd7114d72b87e6b25c9effca0bac16c033b3616
Daniel <danielnimafa@gmail.com>
2018-09-04 11:19:06 +0700

    create fibonacci sequence

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -2,26 +2,51 @@
 
 import UIKit
 
+// fibonacci 0,1,1,2,3,5,8
+
+func fibonacciFun(_ n : Int) {
+    var num1 = 0
+    var num2 = 1
+    
+    var str = "\(num1), \(num2)"
+    
+    for interation in 0...n {
+        let num = num1 + num2
+        str.append(", \(num)")
+        
+        num1 = num2
+        num2 = num
+        
+    }
+    
+    print(str)
+}
+
+fibonacciFun(20)
+
+
+
+
 //for number in 1...20 where number % 2 == 0 {
 //    print(number)
 //}
 
-func bearSong(_ totalNumberOfBottles : Int) -> String {
-    
-    var lyric: String = ""
-    
-    for number in (1...totalNumberOfBottles).reversed() {
-        let newLine = "\(number) bottles of beer on the wall, \(number) bottles of beer.\n Take one down and pass it around, \(number - 1) of beer on the wall.\n\n"
-        lyric += newLine
-    }
-    
-    // lyric += 
-    
-    return lyric
-    
-}
-
-print(bearSong(13))
+//func bearSong(_ totalNumberOfBottles : Int) -> String {
+//
+//    var lyric: String = ""
+//
+//    for number in (1...totalNumberOfBottles).reversed() {
+//        let newLine = "\(number) bottles of beer on the wall, \(number) bottles of beer.\n Take one down and pass it around, \(number - 1) of beer on the wall.\n\n"
+//        lyric += newLine
+//    }
+//
+//    // lyric +=
+//
+//    return lyric
+//
+//}
+//
+//print(bearSong(13))
 
 
 
