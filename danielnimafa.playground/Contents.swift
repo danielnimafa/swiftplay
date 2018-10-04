787ab1d75326db40fddd25274ecd4d3d37db8c31
danielnimafa <danielnimafa@gmail.com>
2018-10-05 05:06:24 +0700

    update

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -1,28 +1,151 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
-// fibonacci 0,1,1,2,3,5,8
+// MARK: - Mutating Functions
+//struct Fruit {
+//    var type: String
+//    var eaten: Bool
+//
+//    init(type: String) {
+//        self.type = type
+//        self.eaten = false
+//    }
+//
+//    mutating func eat() {
+//        eaten = true
+//    }
+//}
+//
+//let apple = Fruit(type: "Apple")
+//apple.eat()
+//
+//var buah = Fruit(type: "Buah")
+//buah.eat()
+//print("Buat eaten: \(buah.eaten)")
 
-func fibonacciFun(_ n : Int) {
-    var num1 = 0
-    var num2 = 1
-    
-    var str = "\(num1), \(num2)"
-    
-    for interation in 0...n {
-        let num = num1 + num2
-        str.append(", \(num)")
-        
-        num1 = num2
-        num2 = num
-        
+
+struct Dog {
+    var name: String
+    var breed: String
+    var age: Int
+    var beenFed: Bool
+
+    init(name: String, breed: String, age: Int) {
+        self.name = name
+        self.breed = breed
+        self.age = age
+        self.beenFed = false
     }
-    
-    print(str)
 }
 
-fibonacciFun(20)
+func feedDog(_ dog : inout Dog) {
+    dog.beenFed = true
+}
+
+
+var milo = Dog(name: "Milo", breed: "Cihuahua", age: 3)
+feedDog(&milo)
+print("Milo beenfed: \(milo.beenFed)")
+
+struct Category {
+
+    private(set) public var title: String
+    private(set) public var imageName: String
+
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
+
+var category = Category(title: "Baju", imageName: "baju.png")
+let namaBaju: String = category.title
+
+var meg = Dog(name: "Meg", breed: "Beagle", age: 5)
+meg.name = "Lucy"
+
+// -------------------- Closure
+
+//class Firebase {
+//
+//    func createUser(username: String, password: String, completion: (Bool, Int) -> Void) {
+//        var isSuccess = true
+//        var userID = 123
+//
+//        completion(isSuccess, userID)
+//    }
+//
+//}
+//
+//class MyApp {
+//
+//    func registerButtonPressed() {
+//
+//        let firebase = Firebase()
+//        firebase.createUser(username: "Bejo", password: "123456") {
+//            (isSuccess: Bool, userID: Int) in
+//
+//        print("Registration is successful: \(isSuccess))")
+//        print("userID is: \(userID)")
+//
+//        }
+//
+//    }
+//
+//    func completed(isSuccess: Bool, userID: Int) {
+//
+//        print("Registration is successful: \(isSuccess))")
+//        print("userID is: \(userID)")
+//
+//    }
+//
+//}
+//
+//let myApp = MyApp()
+//myApp.registerButtonPressed()
+
+
+// -------------------- Array
+//let array = [6,4,2,7,9,1]
+//
+//func addOne(n1: Int) -> Int {
+//    return n1 + 1
+//}
+//
+//print(array.map{$0 + 1})
+
+//array.map({(n1: Int) -> Int in
+//    return n1 + 1
+//})
+
+//array.map(addOne)
+
+// -------------------- Fibonacci
+// fibonacci 0,1,1,2,3,5,8
+
+//func fibonacciFun(_ n : Int) {
+//    var num1 = 0
+//    var num2 = 1
+//
+//    var str = "\(num1), \(num2)"
+//
+//    for _ in 0...n {
+//        let num = num1 + num2
+//        if num % 2 == 0 {
+//            str.append(", \(num)")
+//        }
+//
+//        num1 = num2
+//        num2 = num
+//
+//    }
+//
+//    print(str)
+//}
+//
+//fibonacciFun(30)
 
 
 
@@ -53,7 +176,7 @@ fibonacciFun(20)
 
 
 
-
+// -------------------- Array
 
 //let arrayOfNumber = [12,7,1,3,9,16,19,21]
 //
@@ -75,7 +198,7 @@ fibonacciFun(20)
 
 
 
-
+// -------------------- Calculate BMI
 
 //func calculateBMI(weight: Double, height: Double) -> Double {
 //    //let bmi = weight / (height * height)
