1c55c9f82b82c748c41892d70cf7a9b96e693dda
danielnimafa <danielnimafa@gmail.com>
2018-10-05 05:28:59 +0700

    update

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,6 +3,69 @@
 import UIKit
 import Foundation
 
+// MARK: - Mutating Functions
+//struct Fruit {
+//    var type: String
+//    var eaten: Bool
+//
+//    init(type: String) {
+//        self.type = type
+//        self.eaten = false
+//    }
+//
+//    mutating func eat() {
+//        eaten = true
+//    }
+//}
+//
+//let apple = Fruit(type: "Apple")
+//apple.eat()
+//
+//var buah = Fruit(type: "Buah")
+//buah.eat()
+//print("Buat eaten: \(buah.eaten)")
+
+
+struct Dog {
+    var name: String
+    var breed: String
+    var age: Int
+    var beenFed: Bool
+
+    init(name: String, breed: String, age: Int) {
+        self.name = name
+        self.breed = breed
+        self.age = age
+        self.beenFed = false
+    }
+}
+
+func feedDog(_ dog : inout Dog) {
+    dog.beenFed = true
+}
+
+
+var milo = Dog(name: "Milo", breed: "Cihuahua", age: 3)
+feedDog(&milo)
+print("Milo beenfed: \(milo.beenFed)")
+
+struct Category {
+
+    private(set) public var title: String
+    private(set) public var imageName: String
+
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
+
+var category = Category(title: "Baju", imageName: "baju.png")
+let namaBaju: String = category.title
+
+var meg = Dog(name: "Meg", breed: "Beagle", age: 5)
+meg.name = "Lucy"
+
 // -------------------- Calculate BMI
 
 func calculateBMI(weight: Double, height: Double) -> Double {
