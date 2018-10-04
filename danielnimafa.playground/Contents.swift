607b067aa92eeac917e1c1d4d4f23725b7a455fa
danielnimafa <danielnimafa@gmail.com>
2018-10-05 05:41:28 +0700

    Polymorphism swift

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -4,84 +4,37 @@ import UIKit
 import Foundation
 
 
-// MARK: - Mutating Functions
-struct Fruit {
-    var type: String
-    var eaten: Bool
-
-    init(type: String) {
-        self.type = type
-        self.eaten = false
-    }
-
-    mutating func eat() {
-        eaten = true
+class Shape {
+    var area: Double?
+    
+    func calculateArea(valA: Double, valB: Double) {
+        
     }
 }
 
-let apple = Fruit(type: "Apple")
-apple.eat()
-
-var buah = Fruit(type: "Buah")
-buah.eat()
-print("Buat eaten: \(buah.eaten)")
-
-
-struct Dog {
-    var name: String
-    var breed: String
-    var age: Int
-    var beenFed: Bool
-
-    init(name: String, breed: String, age: Int) {
-        self.name = name
-        self.breed = breed
-        self.age = age
-        self.beenFed = false
+class Triangle: Shape {
+    
+    override func calculateArea(valA: Double, valB: Double) {
+        area = (valA * valB) / 2
+        print("\(area ?? 0.0)")
     }
 }
 
-func feedDog(_ dog : inout Dog) {
-    dog.beenFed = true
-}
-
-
-var milo = Dog(name: "Milo", breed: "Cihuahua", age: 3)
-feedDog(&milo)
-print("Milo beenfed: \(milo.beenFed)")
-
-struct Category {
-
-    private(set) public var title: String
-    private(set) public var imageName: String
-
-    init(title: String, imageName: String) {
-        self.title = title
-        self.imageName = imageName
+class Rectangle: Shape {
+    
+    override func calculateArea(valA: Double, valB: Double) {
+        
+        area = (valA * valB)
     }
 }
 
-var category = Category(title: "Baju", imageName: "baju.png")
-let namaBaju: String = category.title
+class Trapesium: Shape {
+    
+    override func calculateArea(valA: Double, valB: Double) {
+        area = valA + valB
+    }
+}
 
-var meg = Dog(name: "Meg", breed: "Beagle", age: 5)
-meg.name = "Lucy"
-
-//// -------------------- Calculate BMI
-//
-//func calculateBMI(weight: Double, height: Double) -> Double {
-//    //let bmi = weight / (height * height)
-//    let bmi = weight / pow(height, 2)
-//
-//    if bmi > 25 {
-//        print("You are overweight")
-//    } else if bmi > 18.5 && bmi < 25 {
-//        print("You have a normal weight")
-//    } else if bmi < 18.5 {
-//        print("You are underweight")
-//    }
-//
-//    return bmi
-//}
-//
-//var bmiResult = calculateBMI(weight: 70, height: 1.63)
+let tri = Triangle()
+tri.calculateArea(valA: 11.9, valB: 2.5)
+print("Hasil kalkulasi \(tri.area!)")
