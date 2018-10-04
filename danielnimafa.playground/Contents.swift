86d341d910cc74ce2f55a08ff6b0b2947ad83c93
danielnimafa <danielnimafa@gmail.com>
2018-10-05 05:34:11 +0700

    class  vs struct

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,27 +3,28 @@
 import UIKit
 import Foundation
 
+
 // MARK: - Mutating Functions
-//struct Fruit {
-//    var type: String
-//    var eaten: Bool
-//
-//    init(type: String) {
-//        self.type = type
-//        self.eaten = false
-//    }
-//
-//    mutating func eat() {
-//        eaten = true
-//    }
-//}
-//
-//let apple = Fruit(type: "Apple")
-//apple.eat()
-//
-//var buah = Fruit(type: "Buah")
-//buah.eat()
-//print("Buat eaten: \(buah.eaten)")
+struct Fruit {
+    var type: String
+    var eaten: Bool
+
+    init(type: String) {
+        self.type = type
+        self.eaten = false
+    }
+
+    mutating func eat() {
+        eaten = true
+    }
+}
+
+let apple = Fruit(type: "Apple")
+apple.eat()
+
+var buah = Fruit(type: "Buah")
+buah.eat()
+print("Buat eaten: \(buah.eaten)")
 
 
 struct Dog {
@@ -66,21 +67,21 @@ let namaBaju: String = category.title
 var meg = Dog(name: "Meg", breed: "Beagle", age: 5)
 meg.name = "Lucy"
 
-// -------------------- Calculate BMI
-
-func calculateBMI(weight: Double, height: Double) -> Double {
-    //let bmi = weight / (height * height)
-    let bmi = weight / pow(height, 2)
-
-    if bmi > 25 {
-        print("You are overweight")
-    } else if bmi > 18.5 && bmi < 25 {
-        print("You have a normal weight")
-    } else if bmi < 18.5 {
-        print("You are underweight")
-    }
-
-    return bmi
-}
-
-var bmiResult = calculateBMI(weight: 70, height: 1.63)
+//// -------------------- Calculate BMI
+//
+//func calculateBMI(weight: Double, height: Double) -> Double {
+//    //let bmi = weight / (height * height)
+//    let bmi = weight / pow(height, 2)
+//
+//    if bmi > 25 {
+//        print("You are overweight")
+//    } else if bmi > 18.5 && bmi < 25 {
+//        print("You have a normal weight")
+//    } else if bmi < 18.5 {
+//        print("You are underweight")
+//    }
+//
+//    return bmi
+//}
+//
+//var bmiResult = calculateBMI(weight: 70, height: 1.63)
