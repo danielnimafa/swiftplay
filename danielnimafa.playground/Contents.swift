fe42d90657f34c605b3fa7b4dcfcbefb1cc5f19b
danielnimafa <danielnimafa@gmail.com>
2018-11-13 16:48:38 +0700

    learn how to write closure

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,44 +3,30 @@
 import UIKit
 import Foundation
 
-struct Person {
-    let firstName: String
-    let lastName: String
-    let age: Int
+func filterGreaterThanValue(value: Int, numbers: [Int]) -> [Int] {
+    var filteredSetOfNumber = [Int]()
+    for num in numbers {
+        if num > value {
+            filteredSetOfNumber.append(num)
+        }
+    }
+    return filteredSetOfNumber
 }
 
-let peoples = [
-    Person(firstName: "Daniel", lastName: "Prastiwa", age: 28),
-    Person(firstName: "Ramadhania", lastName: "Kayla", age: 3),
-    Person(firstName: "Yas", lastName: "Min", age: 3),
-    Person(firstName: "Daniel", lastName: "Sahuleka", age: 29),
-    Person(firstName: "Daniel", lastName: "Bedingsfield", age: 30),
-    Person(firstName: "Tony", lastName: "Stark", age: 30),
-    Person(firstName: "Chris", lastName: "Hewoth", age: 22),
-    Person(firstName: "Captain", lastName: "America", age: 22),
-    Person(firstName: "Thanos", lastName: "Stone", age: 20)
-]
+//let filteredList = filterGreaterThanValue(value: 5, numbers: [1,2,3,4,5,7,10,20])
+//print(filteredList)
 
-//let groupedDictionary = Dictionary(grouping: peoples) { (person) -> Character in
-//    return person.firstName.first!
-//}
-
-let groupedDictionary = Dictionary(grouping: peoples) { (person) -> String in
-    return person.firstName
+func filterWithPredicate(closure: (Int) -> Bool, numbers: [Int]) -> [Int] {
+    var filteredNumber = [Int]()
+    for num in numbers {
+        if closure(num) {
+            filteredNumber.append(num)
+        }
+    }
+    return filteredNumber
 }
 
-var groupedPeople = [[Person]]()
-
-let keys = groupedDictionary.keys.sorted()
-keys.forEach({
-    groupedPeople.append(groupedDictionary[$0]!)
-})
-
-groupedPeople.forEach({
-    $0.forEach({
-        print($0)
-    })
-    print("----------")
-})
-
-
+let filteredNums = filterWithPredicate(closure: { (num) -> Bool in
+    return num > 2
+}, numbers: [1,2,3,4,5,7,10,20])
+print(filteredNums)
