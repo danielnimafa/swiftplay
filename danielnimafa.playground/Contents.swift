a267cae6b7585a57ac82d81f3cd95517164773da
Daniel Swift <danielnimafa@gmail.com>
2019-02-06 13:12:56 +0700

    learn trailing, capturing, escaping closures

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -30,3 +30,77 @@ let filteredNums = filterWithPredicate(closure: { (num) -> Bool in
     return num > 2
 }, numbers: [1,2,3,4,5,7,10,20])
 print(filteredNums)
+
+
+
+print("\n----------------Trailing closures----------------\n")
+
+let digitNumbers = [0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
+let numbers = [16, 25, 510]
+
+let strings = numbers.map { num -> String in
+    
+    var number = num
+    var output = ""
+    
+    repeat {
+        output = digitNumbers[number % 10]! + output
+        number /= 10
+    } while number > 0
+    
+    return output
+}
+
+print(strings)
+
+
+
+print("\n----------------Capturing Values----------------\n")
+
+func makeIncrementer(forIncrement amount: Int) -> () -> Int {
+    var runningTotal = 0
+    func incrementer() -> Int {
+        runningTotal += amount
+        return runningTotal
+    }
+    
+    return incrementer
+}
+
+let result = makeIncrementer(forIncrement: 3)
+print("Incrementer: \(result())")
+print("Incrementer: \(result())")
+print("Incrementer: \(result())")
+print("Incrementer: \(result())")
+print("Incrementer: \(result())")
+
+
+
+print("\n----------------Escaping closures----------------\n")
+
+var completionHandlers: [() -> Void] = []
+func someFunctionWithEscapingClosure(completionHandler: @escaping () -> Void) {
+    completionHandlers.append(completionHandler)
+}
+
+func someFunctionWithNonescapingClosure(closure: () -> Void) {
+    closure()
+}
+
+class SomeClass {
+    
+    var x = 10
+    
+    func doSomething() {
+        someFunctionWithEscapingClosure { self.x = 100 }
+        someFunctionWithNonescapingClosure { x = 200 }
+    }
+}
+
+let instance = SomeClass()
+instance.doSomething()
+print(instance.x)
+
+completionHandlers.first?()
+print(instance.x)
+
