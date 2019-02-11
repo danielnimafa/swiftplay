5162c657f0e5d1f0d8d19f86c6c3e91ff63e8513
Daniel Swift <danielnimafa@gmail.com>
2019-02-11 23:59:32 +0700

    learning functions
    learn inout, variadic, throwing functions

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,6 +3,65 @@
 import UIKit
 import Foundation
 
+func numberOfTims(in array: [String]) -> Int {
+    var count = 0
+    for name in array {
+        if name == "Tim" {
+            count += 1
+        }
+    }
+    return count
+}
+
+
+
+print("--------------------- inout parameters  ---------------------")
+
+func doubleInPlace(number: inout Int) {
+    number *= 2
+}
+
+var myNum = 10
+doubleInPlace(number: &myNum)
+print(myNum)
+
+
+print("--------------------- writing Throwing Functions  ---------------------")
+
+enum PasswordError: Error {
+    case obvious
+}
+
+func checkPassword(_ password: String) throws -> Bool {
+    if password == "password" {
+        throw PasswordError.obvious
+    }
+    
+    return true
+}
+
+let pass = "password"
+do {
+    try checkPassword(pass)
+} catch let err {
+    print(err)
+}
+
+
+
+
+print("--------------------- Variadic Functions ---------------------")
+
+func square(numbers: Int...) {
+    for number in numbers {
+        print("\(number) squared is \(number * number)")
+    }
+}
+
+square(numbers: 2,4,5,6,3,1)
+
+
+
 let intArray = [2,5,7,3,4,29,56,72,34,55,333]
 let stringArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
 
