cd243a0fc7990e2cb5f516604cbbb1e5cfd7c8b7
Daniel Swift <danielnimafa@gmail.com>
2019-03-06 05:24:57 +0700

    Final classes

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,6 +3,23 @@
 import UIKit
 import Foundation
 
+// -------------------- Final Classes
+
+final class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+class LittleDog : Dog {
+    
+}
+
+
 
 class Shape {
     var area: Double?
