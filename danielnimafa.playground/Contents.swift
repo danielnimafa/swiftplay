3035f13b796857562d5b11d3e61e8fce9baedab7
Daniel Swift <danielnimafa@gmail.com>
2019-03-06 05:30:57 +0700

    Copying Objects

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,6 +3,30 @@
 import UIKit
 import Foundation
 
+
+// -------------------- Copying Objects
+// The third difference between classes and structs is how they are copied. When you copy a struct, both the original and the copy are different things – changing one won’t change the other. When you copy a class, both the original and the copy point to the same thing, so changing one does change the other.
+
+struct SingerStruct {
+    var name = "Taylor Swift"
+}
+
+var singerStruct = SingerStruct()
+print(singerStruct.name)
+var copySingerStruct = singerStruct
+copySingerStruct.name = "Britney Spears"
+print(singerStruct.name)
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+var copySinger = singer
+copySinger.name = "Taylor Kotlin"
+print(singer.name)
+
 // -------------------- Final Classes
 
 final class Dog {
