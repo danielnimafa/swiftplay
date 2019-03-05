64345d19935761deb71437c7e6aead4a308e34d3
Daniel Swift <danielnimafa@gmail.com>
2019-03-05 07:45:52 +0700

    Properties and methods of strings

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -3,6 +3,31 @@
 import UIKit
 import Foundation
 
+// ------------------------------ Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+
+// ------------------------------ Mutating Methods
+
+struct Person {
+    var name: String
+    
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Joko")
+person.makeAnonymous()
+print(person.name)
+
+
 // ------------------------------ Methods
 
 struct City {
@@ -17,3 +42,18 @@ let london = City(population: 9_000_000)
 let taxes = london.collectTaxes()
 print(taxes)
 
+// ------------------------- Property observer
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
