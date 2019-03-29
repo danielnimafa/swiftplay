d7827944a1b689446186b45a4914c4ecec8a6326
danielnimafa <danielnimafa@gmail.com>
2019-03-29 10:06:29 +0700

    learn lazy property

--- danielnimafa.playground/Contents.swift
+++ danielnimafa.playground/Contents.swift
@@ -4,6 +4,32 @@ import UIKit
 import Foundation
 
 
+// -------------------- Lazy Properties
+
+class Computer {
+    var power: Int
+    var speed: Int
+    var ram: Int
+    
+    lazy var cpuPower: Int = {
+        return (1000 * 2) + (2000 * 2) + (3000 * 2) + 1000
+    }()
+    
+    var computerOverallValue: Int {
+        return power * speed * ram + cpuPower
+    }
+    
+    init(power: Int, speed: Int, ram: Int) {
+        self.power = power
+        self.speed = speed
+        self.ram = ram
+    }
+}
+
+let myComputer = Computer(power: 300, speed: 200, ram: 700)
+print(myComputer.computerOverallValue)
+
+
 // -------------------- Mutability
 // The final difference between classes and structs is the way they deal with constants. If you have a constant struct with a variable property, that property can’t be changed because the struct itself is constant.
 
